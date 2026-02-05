44cfeca5dbc1adffd0a632cc816115f4191a5aa7
Rajesh kumar <rajeshkumarkarri3434@gmail.com>
2026-02-05 15:42:18 +0530

    commit

--- BootMVCProj07-RequestParamsBinding/src/main/java/com/raj/controller/StudentOperationsContrller.java
+++ BootMVCProj07-RequestParamsBinding/src/main/java/com/raj/controller/StudentOperationsContrller.java
@@ -1,5 +1,8 @@
 package com.raj.controller;
 
+import java.util.Arrays;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,21 +12,45 @@ public class StudentOperationsContrller {
 
 	/*@GetMapping("/data")
 	public String readData(@RequestParam("sno")int no, @RequestParam("sname")String name) {
-		
+
 		System.out.println("sno="+no+" ... sname="+name);
-		
+
 		//return LVN
 		return "show-data";
 	}*/
-	
-	@GetMapping("/data")
-	public String readData(@RequestParam(required = false, defaultValue = "3001") Integer sno, 
+
+	/*@GetMapping("/data")
+	public String readData(@RequestParam(required = false) Integer sno, 
 			               @RequestParam(required = false,defaultValue = "VARANASI") String sname) {
-		
+
 		System.out.println("sno="+sno+" ... sname="+sname);
-		
+
+		//return LVN
+		return "show-data";
+	}*/
+
+	/*@GetMapping("/data")
+	public String readData(@RequestParam(required = false, defaultValue = "3001") Integer sno, 
+			               @RequestParam(required = false,defaultValue = "VARANASI") String sname,
+			               @RequestParam String[] sadd,
+			               @RequestParam Set<String> namesSet) {
+
+		System.out.println("sno="+sno+" ... sname="+sname+" ... "+Arrays.toString(sadd)+" ... namesSet="+namesSet);
+
 		//return LVN
 		return "show-data";
 	}
+	 */	
+	@GetMapping("/data")
+	public String readData(@RequestParam(required = false, defaultValue = "3001") Integer sno, 
+			@RequestParam(required = false,defaultValue = "VARANASI") String sname,
+			@RequestParam String[] sadd) {
+
+		System.out.println("sno="+sno+" ... sname="+sname+" ... "+Arrays.toString(sadd));
+
+		//return LVN
+		return "show-data";
+	}
+
 
 }
--- BootMVCProj07-RequestParamsBinding/src/main/webapp/WEB-INF/pages/show-data.jsp
+++ BootMVCProj07-RequestParamsBinding/src/main/webapp/WEB-INF/pages/show-data.jsp
@@ -1,5 +1,22 @@
 <%@ page isELIgnored="false"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
 <h1 style="color:red; text-align:center">Result Page</h1>
 <br><br>
-<b>Request Param attributes:: ${param.sno}, ${param.sname}</b>
+<b>Request Param attributes:: ${param.sno}, ${param.sname} </b>
+<br><br>
+<b>Student Address:: </b><br>
+<c:if test="${not empty paramValues.sadd}">
+	<c:forEach var="add" items="${paramValues.sadd}">
+		${add}<br>
+	</c:forEach>
+</c:if>
+
+<br><br>
+
+<b>Names::</b><br>
+<c:if test="${not empty paramValues.namesSet}">
+	<c:forEach var="name" items="${paramValues.namesSet}">
+		${name}<br>
+	</c:forEach>
+</c:if>

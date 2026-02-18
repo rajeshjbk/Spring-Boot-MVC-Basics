2881d2ff2c8dfd8255f8b2a7c49ca3422af3836a
Rajesh kumar <rajeshkumarkarri3434@gmail.com>
2026-02-18 13:59:16 +0530

    first-commit

---
+++ BootMVCProj15-MiniProject-Thymeleaf/src/main/java/com/raj/controller/HomeOperationsController.java
@@ -0,0 +1,14 @@
+package com.raj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeOperationsController {
+
+	@GetMapping("/")
+	public String showHome() {
+		
+		return "redirect:/doctor/";
+	}
+}
